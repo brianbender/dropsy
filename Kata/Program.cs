42b2a732be1105f5ac24857135bd05268b8afb1f
mattwesthoff <matt@enlearn.org>
2016-05-09 13:44:20 -0700

    moving delay back to .5 sec

--- Kata/Program.cs
+++ Kata/Program.cs
@@ -8,7 +8,7 @@ namespace Kata
         {
             IRandomGenerator randomGenerator = new RandomGenerator();
             var board = new Board(9, randomGenerator);
-            var game = new GameController(board, new ConsoleWrapper(), 1000);
+            var game = new GameController(board, new ConsoleWrapper(), 500);
             while (!game.GameIsOver)
             {
                 game.DisplayBoard();
